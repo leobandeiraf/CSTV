5250218e1dabb9196dadf82259aebff25df9c5cf
Leonardo Bandeira <leobandeiraf@gmail.com>
2024-08-20 10:43:40 -0300

    Initial commit

---
+++ CSTV/Scenes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CSTV
+//
+//  Created by Leonardo Bandeira on 20/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
