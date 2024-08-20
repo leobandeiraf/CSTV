4f19296b47fe0d788898cbf89add5fac84798f56
Leonardo Bandeira <leobandeiraf@gmail.com>
2024-08-20 14:26:21 -0300

    Added tableview to matchlist

--- CSTV/Scenes/MatchList/MatchListViewContoller.swift
+++ CSTV/Scenes/MatchList/MatchListViewContoller.swift
@@ -1,9 +1,15 @@
+import SnapKit
 import UIKit
 
 final class MatchListViewContoller: UIViewController {
     // MARK: - Property(ies).
+    enum Section: CaseIterable { case matches }
     private let coordinator: MatchListCoordinating
     private let viewModel: MatchListViewModeling
+    
+    // MARK: - Component(s).
+    private lazy var tableView = TableView<Section>()
+        .backgroundColor(.backgroundColor ?? .black)
     
     // MARK: - Initialization.
     init(coordinator: MatchListCoordinating, viewModel: MatchListViewModeling) {
@@ -18,6 +24,39 @@ final class MatchListViewContoller: UIViewController {
     // MARK: - Override(s).
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        buildLayout()
+    }
+    
+    // MARK: - Method(s).
+    private func buildLayout() {
+        buildViewHierarchy()
+        setupConstraints()
+        configureViews()
+    }
+    
+    private func buildViewHierarchy() {
+        view.addSubview(tableView)
+    }
+    
+    private func setupConstraints() {
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    private func configureViews() {
+        title = "Partidas"
+        configureNavigationBar()
+    }
+    
+    private func configureNavigationBar() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        navigationController?.navigationBar.barTintColor = UIColor.backgroundColor
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+        let backImage = UIImage(named: "ic-arrow-left")
+        navigationController?.navigationBar.backIndicatorImage = backImage
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = backImage
+        navigationController?.navigationBar.tintColor = .white
     }
 }
