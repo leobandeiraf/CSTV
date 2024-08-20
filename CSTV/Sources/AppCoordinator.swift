34923baff262e60c998eebd3ae9901b2afc3d05d
Leonardo Bandeira <leobandeiraf@gmail.com>
2024-08-20 12:06:35 -0300

    Create AppCoordinator.swift

---
+++ CSTV/Sources/AppCoordinator.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+protocol Coordinating: AnyObject {
+    var navigationController: UINavigationController { get set }
+    var rootViewController: UIViewController? { get set }
+    
+    func start()
+}
+
+final class AppCoordinator: Coordinating {
+    // MARK: - Property(ies).
+    var navigationController: UINavigationController
+    var rootViewController: UIViewController?
+    
+    // MARK: - Initialization.
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    // MARK: - Method(s).
+    func start() {
+        let coordinator = MatchListCoordinator(navigationController: navigationController)
+        coordinator.start()
+    }
+}
