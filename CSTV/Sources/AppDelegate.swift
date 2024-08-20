276d3ce9c1d8ed829a3788081b9b8184753264c3
Leonardo Bandeira <leobandeiraf@gmail.com>
2024-08-20 11:52:30 -0300

    Update AppDelegate.swift

--- CSTV/Sources/AppDelegate.swift
+++ CSTV/Sources/AppDelegate.swift
@@ -6,7 +6,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = ViewController()
+        let viewModel = MatchListViewModel()
+        let viewController = MatchListViewContoller(viewModel: viewModel)
+        window?.rootViewController = viewController
         window?.makeKeyAndVisible()
         return true
     }
