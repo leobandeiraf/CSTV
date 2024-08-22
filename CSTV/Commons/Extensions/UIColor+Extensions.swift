d2e145c55087db473bc228ea7b0eb75e7d51e466
Leonardo Bandeira <leobandeiraf@gmail.com>
2024-08-22 17:15:27 -0300

    Delete UIColor+Extensions.swift

--- CSTV/Commons/Extensions/UIColor+Extensions.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-extension UIColor {
-    static let backgroundColor = UIColor(named: "background-color")
-    static let neutral01 = UIColor(named: "neutral-color-01")
-    static let neutral02 = UIColor(named: "neutral-color-02")
-    static let highlightColor = UIColor(named: "highlight-color")
-}
