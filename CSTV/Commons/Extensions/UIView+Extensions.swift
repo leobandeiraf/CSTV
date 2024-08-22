696f2eea0cd3762f4e12ad41e8df469fec739bce
Leonardo Bandeira <leobandeiraf@gmail.com>
2024-08-22 17:15:43 -0300

    Update UIView+Extensions.swift

--- CSTV/Commons/Extensions/UIView+Extensions.swift
+++ CSTV/Commons/Extensions/UIView+Extensions.swift
@@ -4,4 +4,15 @@ extension UIView {
     func addSubviews(_ views: UIView...) {
         views.forEach { addSubview($0) }
     }
+    
+    func corner(radius: CGFloat, corners: UIRectCorner = .allCorners) {
+        clipsToBounds = true
+        layer.cornerRadius = radius
+        var masked = CACornerMask()
+        if corners.contains(.topLeft) { masked.insert(.layerMinXMinYCorner) }
+        if corners.contains(.topRight) { masked.insert(.layerMaxXMinYCorner) }
+        if corners.contains(.bottomLeft) { masked.insert(.layerMinXMaxYCorner) }
+        if corners.contains(.bottomRight) { masked.insert(.layerMaxXMaxYCorner) }
+        self.layer.maskedCorners = masked
+    }
 }
